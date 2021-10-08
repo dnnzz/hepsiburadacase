a263ce0defba3b0d06a1e10c0b597afbd30c65f0
dnnzz <l1611012074@stud.sdu.edu.tr>
2021-10-08 16:15:50 +0300

    fix/deployissues

--- src/index.js
+++ src/index.js
@@ -2,10 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { products } from "./data/data";
-
-// set product data to localstorage on start
-// window.localStorage.setItem("productList", JSON.stringify(products));
-// window.localStorage.setItem("cart", JSON.stringify([]));
 
 ReactDOM.render(<App />, document.getElementById("root"));
